ed2a929c8112e531241291e3165ef58ceee7fff4
Tolgay Özdemir <tolgay.oezdemir@hsrw.org>
2024-10-28 15:23:35 +0100

    node-express-2-1

---
+++ 01-Node-Express-Level-2-1/01-backend/src/index.ts
@@ -0,0 +1,72 @@
+import express from 'express'
+import { IStarship } from './models/IStarship'
+import { IPerson } from './models/IPerson'
+import cors from 'cors'
+import path from 'path'
+
+const app = express()
+const port = 3000
+app.use(cors())
+app.use(express.static(path.join(__dirname, "public")))
+
+
+const starships: IStarship[] = [
+    {
+        id: 1, 
+        name: "TIE Fighter",
+    },
+    {
+        id: 2,
+        name: "Foe Hammer",
+        img: "/assets/Foe_Hammer_Pelican.webp"
+    },
+    {
+        id: 3,
+        name: "UNSC Infinity",
+        img: "/assets/otwsxml8wcx21.webp"
+    },
+    {
+        id: 4,
+        name: "The Oracle"
+    },
+    {
+        id: 5,
+        name: "Firefly"
+    }
+]
+
+const persons: IPerson[] = [
+    {
+        id: 117,
+        name: "Master Chief"
+    },
+    {
+        id: 2,
+        name: "Han Solo"
+    },
+    {
+        id: 3,
+        name: "Leia Organa"
+    },
+    {
+        id: 4,
+        name: "Buzz Lightyear"
+    },
+    {
+        id: 5,
+        name: "Arbiter"
+    }
+]
+
+
+app.get("/starships", (req, resp)=>{
+    resp.json(starships)
+})
+
+// app.get("/person", (req, resp)=>{
+//     resp.json(persons)
+// })
+
+app.listen(port, ()=>{
+    console.log(`Server running at ${port}`)
+})
---
+++ 01-Node-Express-Level-2-1/01-frontend/src/App.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react'
+
+export interface IStarship {
+  id: number;
+  name: string;
+  img?: string;
+}
+
+function App() {
+  const [starshipData, setStarshipData] = useState<IStarship[]>([])
+  useEffect(()=>{
+    fetch("http://localhost:3000/starships")
+    .then((response)=> response.json())
+    .then((json)=>setStarshipData(json))
+  },[])
+
+  return (
+    <div>
+    <h1>Hey!</h1>
+    {starshipData.map((starship) => (
+      <div key={starship.id}>
+        <img src={starship.img} alt={starship.name} />
+        <h3>{starship.name}</h3>
+      </div>
+    ))}
+  </div>
+  )
+}
+
+export default App
