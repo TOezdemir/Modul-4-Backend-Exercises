6f811c52d8b1ab0f6a2bfa01e3bff5f44a04f704
Tolgay Özdemir <tolgay.oezdemir@hsrw.org>
2024-10-31 13:34:27 +0100

    initiated supabase

---
+++ 03-supabase-project-recipebook/src/App.tsx
@@ -0,0 +1,19 @@
+import Header from "./components/Header"
+import Hero from "./components/Hero"
+import FeaturedRecipes from "./components/FeaturedRecipies"
+import Footer from "./components/Footer"
+
+
+function App() {
+
+  return (
+    <div>
+      <Header/>
+      <Hero/>
+      <FeaturedRecipes/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
