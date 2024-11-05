83380200c23d9beb9b81cac1ff75714c681e6de0
Tolgay Özdemir <tolgay.oezdemir@hsrw.org>
2024-11-05 15:03:53 +0100

    routing done

--- 03-supabase-project-recipebook/src/components/Layout.tsx
+++ 03-supabase-project-recipebook/src/components/Layout.tsx
@@ -1,13 +1,17 @@
 import Header from "./Header"
 import Hero from "./Hero"
 import Footer from "./Footer"
+import { Outlet } from "react-router-dom"
+import FeaturedRecipes from "./FeaturedRecipes"
+
 
 export default function Layout(){
     return(
         <>
         <Header/>
         <Hero/>
-        
+        <FeaturedRecipes/>
+        <Outlet/>
         <Footer/>
         </>
     )
