6b4088c1d4ef962848880a5264810362c36069f8
Tolgay Özdemir <tolgay.oezdemir@hsrw.org>
2024-11-06 09:41:34 +0100

    dont know what I am doing

--- 03-supabase-project-recipebook/src/components/Layout.tsx
+++ 03-supabase-project-recipebook/src/components/Layout.tsx
@@ -1,10 +1,16 @@
+import Header from "./Header"
+import Hero from "./Hero"
+import Footer from "./Footer"
 import { Outlet } from "react-router-dom"
 
 
 export default function Layout(){
     return(
         <>
+        <Header/>
+        <Hero/>
         <Outlet/>
+        <Footer/>
         </>
     )
 }
--- 03-supabase-project-recipebook/src/pages/Home.tsx
+++ 03-supabase-project-recipebook/src/pages/Home.tsx
@@ -1,15 +1,15 @@
-import FeaturedRecipes from "../components/FeaturedRecipes"
-import Footer from "../components/Footer"
-import Header from "../components/Header"
-import Hero from "../components/Hero"
+// import FeaturedRecipes from "../components/FeaturedRecipes"
+// import Footer from "../components/Footer"
+// import Header from "../components/Header"
+// import Hero from "../components/Hero"
 
 export default function Home(){
     return(
         <>
-        <Header/>
+        {/* <Header/>
         <Hero/>
         <FeaturedRecipes/>
-        <Footer/>
+        <Footer/> */}
         </>
     )
 }
